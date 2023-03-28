9f85dec4004b1817335242b3a79518651915c63f
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-03-28 21:46:26 +0100

    fixed pycodestyle errors on 001"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,12 +2,13 @@
 """ a module on creating base cases
 """
 
+
 class Base:
     """ a class named base """
     __nd_objects = 0
 
     def __init__(self, id=None):
-        if id == None:
+        if id is None:
             Base.__nd_objects += 1
             self.id = Base.__nd_objects
         else:
