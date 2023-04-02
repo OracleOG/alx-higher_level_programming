a81335a90b060e52f495fd4bbcd9816ad3cabad0
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-04-02 15:20:14 +0100

    fixing project 2 001

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a module tha defines the class rectnagle """
-from module.base import Base
+from models.base import Base
 
 class Rectangle(Base):
     """ a class that defines a rectangle. """
