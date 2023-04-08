293486b905564118426f972fb2f38877fb280466
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-04-08 14:26:03 +0100

    fixing 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation of list_dictionaries"""
         if list_dictionaries == None or len(list_dictionaries) == 0:
             return '[]'
         return json.dumps(list_dictionaries)
