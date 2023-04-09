f5e17ff6b5ebefae080bdd6ee9e8743227de87ed
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-04-09 13:09:14 +0100

    updating files

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """ createss an object from a JSON file """
-    with open('filename', 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         decoded_json = json.load(f)
         return decoded_json
