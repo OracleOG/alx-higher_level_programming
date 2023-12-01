b9828f70087205d3614e43455033130a357f47ec
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-01 01:27:53 +0100

    fix the "correct output - wrong type" bug on the code 002

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-number = 98
-if not isinstance(number, int):
-    raise ValueError("Variable 'number' must be an integer")
-print(f"{number} Battery street")
+number = '98'
+print(f"{number = int(number)} Battery street")
