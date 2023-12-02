aa8de25da74a9d57bce7087cf2c382bc134b594e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-02 01:51:50 +0100

    change method of printing out all the lower alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-
-for ch in range(97,123):
+#!/usr/bin/python3
+for i in range(97, 123):
+    print(chr(i), end='')
+'''
+for ch in range(97, 123):
     print("{:c}".format(ch), end='')
+'''
