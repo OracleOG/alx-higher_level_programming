8b06450074ed00dd65ce9336f6104b478ac8297e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-02 18:24:35 +0100

    print string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+
+    new_str = ''
+    for ch in str:
+        asval = ord(ch)
+        if asval >= 97 and asval <= 122:
+            x = (asval - 96) + 64
+            newchar = chr(x)
+            new_str = new_str + newchar
+        else:
+            new_str = new_str + ch
+    print(new_str)
