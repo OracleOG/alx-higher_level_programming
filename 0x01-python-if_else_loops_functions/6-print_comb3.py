508e4d586ccba3b04c115e1da2273846d6ed3c76
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-02 11:25:47 +0100

    print combinations of number from 1-9

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    num = i + 1
+    for x in range(num, 10):
+        if i != 8:
+            print("{:d}{:d}".format(i, x), end='')
+            print(",", end="")
+        else:
+            print("{:d}{:d}".format(i, x))
