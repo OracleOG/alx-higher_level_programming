b6d9019502163c3c174197d65066904aca6d99d3
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-02 01:37:32 +0100

    remove whitespaces from keywords and ends file with a new line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -14,10 +14,9 @@ print(f"Last digit of {number} is {l_digit}", end=' ')
 # and is less than 6 and not 0
 
 
-if(l_digit > 5):
+if (l_digit > 5):
     print(f"and is greater than 5")
-elif(l_digit == 0):
+elif (l_digit == 0):
     print(f"and is 0")
-elif(l_digit < 6 and l_digit != 0):
+elif (l_digit < 6 and l_digit != 0):
     print(f"and is less than 6 and not 0")
-    
