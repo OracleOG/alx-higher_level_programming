c7fca60868f35534575d590e9b306327be8cfa8b
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-06 22:25:47 +0100

    print highest integer in the list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    x = 0
+    for row in my_list:
+        if x < row:
+            x = row
+    return x
