8f300c8da1061d8a283e80dc9fbd4a3b5945e71e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-06 22:46:14 +0100

    find multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    new = []
+    for row in my_list:
+        if row % 2 == 0:
+            new.append(True)
+        elif row % 2 == 1:
+            new.append(False)
+    return new
