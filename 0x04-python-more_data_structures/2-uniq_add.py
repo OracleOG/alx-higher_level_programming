331a21dbd40ca84839f84755ef7c910d40fb9725
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-10 13:12:05 +0100

    add unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
-
 def uniq_add(my_list=[]):
-    no_dup = set(my_list)
-
-    num = 0
-
-    for x in my_list:
-        num += x
-
-    return (num)
+    new_list = list(set(my_list))
+    sum = 0
+    for x in new_list:
+        sum = sum + x
+    return sum
