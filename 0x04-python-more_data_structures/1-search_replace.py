4c1fad9c006ceaf79840d512bf249da116c9be67
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-10 12:28:19 +0100

    replace all occurences of an element with another

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
 def search_replace(my_list, search, replace):
-    n_list = list(map(lambda x: replace if x == search else x, my_list))
-
-    return(n_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
