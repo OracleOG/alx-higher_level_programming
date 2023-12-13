ca742ef14eefe6617f0e638883902f1a9108d8b9
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-13 07:46:59 +0100

    correct pycodestyle error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+def print_sorted_dictionary(a_dictionary):
+    s_keys = list(sorted(a_dictionary))
+    for keys in s_keys:
+        print(f"{keys}: {a_dictionary[keys]}")
 
+
+'''
 def print_sorted_dictionary(a_dictionary):
     new_list = list(a_dictionary.keys())
     new_list.sort()
     for i in new_list:
         print("{}: {}" .format(i, a_dictionary.get(i)))
+'''
