49779f2a9fe8bcee677dd2bc11d83ae850039e6f
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-16 11:35:01 +0100

    fix pycodestyle error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,24 +3,11 @@
 def best_score(a_dictionary):
     x = 0
     dicts = {'begin': 0}
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     for keys, val in a_dictionary.items():
         if dicts['begin'] < val:
             x = keys
             dicts['begin'] = val
-        
+
     return x
-
-'''
-def best_score(a_dictionary):
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        for x in list_keys:
-            if a_dictionary[i] >= a_dictionary
-            best = i
-            break
-
-    return (best)
-'''
