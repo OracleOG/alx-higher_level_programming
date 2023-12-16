1757c5688307fd85a1ec6e080004ab72c7b175dc
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-16 12:32:20 +0100

    allow subtractive notations for roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    # Define a dict, where the key = roman figures, while the 'values' = integers
+    # Define a dict, where the key = roman figures,
+    # while the 'values' = integers
     code = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
+    strlen = len(roman_string) - 1
     # loop through the string to identify each char of the roman numeral.
     # - use a condition to check if "roman_string" = str or "None" value.
     # - add up value in the str
-    for rom in roman_string:
-        num = num + code[rom]
+    for x, rom in enumerate(roman_string):
+        if x < strlen:
+            if code[rom] < code[roman_string[x + 1]]:
+                num = num - code[rom]
+            else:
+                num = num + code[rom]
+        else:
+            num = num + code[rom]
     return num
