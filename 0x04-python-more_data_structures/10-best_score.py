4db735d4d2a57ab122d7b9c3a54e3137ca7718f7
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-16 11:24:09 +0100

    return key of the biggest value in a dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
+def best_score(a_dictionary):
+    x = 0
+    dicts = {'begin': 0}
+    if a_dictionary == None:
+        return None
+    for keys, val in a_dictionary.items():
+        if dicts['begin'] < val:
+            x = keys
+            dicts['begin'] = val
+        
+    return x
 
+'''
 def best_score(a_dictionary):
     list_keys = list(a_dictionary.keys())
 
@@ -11,3 +23,4 @@ def best_score(a_dictionary):
             break
 
     return (best)
+'''
