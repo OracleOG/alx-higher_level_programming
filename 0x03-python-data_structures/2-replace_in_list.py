dfbb51ddd4c4064ea2a298b01080f9942b8fbcf0
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 05:54:00 +0100

    return original list if idx is less than zero

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return (None)
+        return (my_list)
     x = len(my_list)
     if idx > (x - 1):
-        return (None)
+        return (my_list)
     my_list[idx] = element
     return (my_list)
