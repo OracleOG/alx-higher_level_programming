bcd20c78474827525e5d68d6118006722e67fa4c
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 05:38:59 +0100

    behaviour when feed with a None or value that isnt a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,10 @@
 def roman_to_int(roman_string):
     # Define a dict, where the key = roman figures,
     # while the 'values' = integers
+    if roman_string is None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
     code = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     strlen = len(roman_string) - 1
@@ -10,8 +14,7 @@ def roman_to_int(roman_string):
     # - use a condition to check if "roman_string" = str or "None" value.
     # - add up value in the str
     for x, rom in enumerate(roman_string):
-        if not isinstance(roman_string, (str, None)):
-            return 0
+        
         if x < strlen:
             if code[rom] < code[roman_string[x + 1]]:
                 num = num - code[rom]
