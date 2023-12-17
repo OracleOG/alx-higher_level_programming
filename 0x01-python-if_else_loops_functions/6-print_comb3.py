89cac38d075d70e23cef141acbc28488c3f3de8e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 11:27:50 +0100

    add space before the integer and fix pycode error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(0, 9):
     num = i + 1
     for x in range(num, 10):
         if i != 8:
-            print("{:d}{:d}".format(i, x), end='')
-            print(" ,", end="")
+            print(" {:d}{:d}".format(i, x), end='')
+            print(",", end="")
         else:
             print("{:d}{:d}".format(i, x))
