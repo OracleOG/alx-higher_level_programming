a6a77117fe02c1ef001f09ce3322aaae78c941df
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 11:33:38 +0100

    add space around integer

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(0, 9):
             print("{:d}{:d}".format(i, x), end='')
             print(", ", end="")
         else:
-            print("{:d}{:d} ".format(i, x))
+            print("{:d}{:d}".format(i, x))
