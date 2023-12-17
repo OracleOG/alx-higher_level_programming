3d1b2ce64e028685f78ae9d06832263800f8a925
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 05:59:02 +0100

    print integer in a string all seperated by a new line

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    x = len(my_list) - 1
-    while x >= 0:
-        print("{}".format(my_list[x]))
-        x -= 1
+    for x in my_list:
+        print("{}".format(x))
