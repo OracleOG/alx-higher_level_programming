fa5a94728fd0d32d5590f900cf68af6c15f5db9e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 09:59:09 +0100

    handles an empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     row = len(matrix)
     for row in matrix:
         N_col = len(row) - 1
@@ -8,5 +10,5 @@ def print_matrix_integer(matrix=[[]]):
             if N_col == x:
                 print("{:d}".format(col))
             else:
-                print("{:d} ".format(col), end = '')
+                print("{:d} ".format(col), end='')
             x += 1
