62a7a7ddf7b6937e8238a59224ace1a629b6b066
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-17 10:22:42 +0100

    handle empty sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     leng = len(sentence)
-    first = sentence[0]
+    if leng == 0:
+        first = None
+    else:
+        first = sentence[0]
     return (leng, first)
