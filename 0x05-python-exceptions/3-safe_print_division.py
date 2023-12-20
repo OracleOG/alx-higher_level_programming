544c2c0524c0152b1edbf8bd80efb180c10e9931
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-21 00:51:10 +0100

    assign the value of None to the RESULT variable on the EXCEPT clause

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,6 +2,18 @@
 
 
 def safe_print_division(a, b):
+
+    try:
+        c = a / b
+    except (TypeError, ValueError, ZeroDivisionError):
+        c = None
+        return c
+    finally:
+        print("Inside result: {}".format(c))
+        return c
+
+'''        
+def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
@@ -9,3 +21,4 @@ def safe_print_division(a, b):
     finally:
         print("Inside result:{}".format(result))
         return (result)
+'''
