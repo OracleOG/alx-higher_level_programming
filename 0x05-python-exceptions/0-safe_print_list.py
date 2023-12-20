40383052d86f2507498a035663a8df285501fdd6
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-20 14:34:07 +0100

    print content of a list and return the number of char printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
+    for num in range(x):
+        try:
+            print(my_list[num], end='')
+        except IndexError:
+            print()
+            return (num)    
+    print()
+    return (num + 1)
+
+'''def safe_print_list(my_list=[], x=0):
 #use try to printout of num
     y = 0
     for num in range(x):
@@ -13,3 +22,4 @@ def safe_print_list(my_list=[], x=0):
             y += 1
     print()
     return (y)
+'''
