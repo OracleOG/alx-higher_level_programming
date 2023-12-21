d86ccd95e9e50fb0597e3979573c60506fa9d75d
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-21 01:06:48 +0100

    fix structure of code and pycodestyle error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,20 +5,9 @@ def safe_print_division(a, b):
 
     try:
         c = a / b
+        return c
     except (TypeError, ValueError, ZeroDivisionError):
         c = None
         return c
     finally:
         print("Inside result: {}".format(c))
-        return c
-
-'''        
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result:{}".format(result))
-        return (result)
-'''
