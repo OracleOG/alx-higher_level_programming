574a216cedcae50204328664a3879c1019eabc1a
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-22 20:08:18 +0100

    fix docstring on module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A module to test the functionality of the python "class" feature
 
+This module contains a simple class called Square
 """
 class Square:
 """An empty class object"""
