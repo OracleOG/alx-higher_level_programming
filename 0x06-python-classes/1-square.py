a2cff20cedb9fa5ec69081dfa1a9455f640d9358
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-28 05:40:06 +0100

    fix docstring error 001

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 
 
-""" A module to test the functionality of the python "class" feature
+"""A module to test the functionality of the python "class" feature
 
-This module contains the definition of a Class Square """
+This module contains the definition of a Class Square"""
 
 class Square:
-""" Defines the object class.
+    """Defines the object class.
 
     Args:
-        param (int): size of the square -  has no type and no value verification is done on it.
-"""
+    param (int): size of the square -  has no type and no value verification is done on it."""
     def __init__(self, size):
         self.__size = size
-'''        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 
 class square:
     """ class square that defines a square object
