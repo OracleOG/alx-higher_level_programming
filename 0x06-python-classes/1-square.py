11051594fe0a7521e52fd887563d6cba9f4c62d4
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-28 05:16:49 +0100

    create a class square with a private instance attribute SIZE

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 
+""" A module to test the functionality of the python "class" feature
+
+This module contains the definition of a Class Square """
+
+class Square:
+""" Defines the object class.
+
+    Args:
+        param (int): size of the square -  has no type and no value verification is done on it.
+"""
+    def __init__(self, size):
+        self.__size = size
+'''        
+
 class square:
     """ class square that defines a square object
 
@@ -12,3 +26,4 @@ class square:
         param1 (int): size of the square
         """
         self.size = size
+'''
