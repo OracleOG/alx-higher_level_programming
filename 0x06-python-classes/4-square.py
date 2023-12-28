cc954a42c42db521399e3dbfec667ebd2ed14e2f
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-28 16:47:14 +0100

    add a property and a property setter to the attribute size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 
 
-class Square:
-    """ a class that defines the size of of a square
-    """
-    def __init__(self, size=0):
+"""A module to test the functionality of the python "class" feature
 
+This module contains the definition of a Class Square"""
+
+class Square:
+    """ Defines the object class.
+
+    Args:
+        size - is of no type and no value verification is done on it.
+        """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """getter function for the attribute SIZE"""
+        return self.__size
+    
+    @size.setter
+    def size(self, size):
+        """Setter function for the attribute SIZE"""
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size
 
     def area(self):
-            """ method that returns the area of the square
-            """
-            self.__size ** 2
-
-    @property
-
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            
+        """method that returns the square area of the object
+        Args:
+            No Args
+        """
+        return self.__size ** 2
