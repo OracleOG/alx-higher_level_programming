f6f4fff58741ebc64aff5dec0767f169fa7fc27f
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2023-12-28 17:52:08 +0100

    add the position attribute to the my_print function

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,6 +67,8 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
