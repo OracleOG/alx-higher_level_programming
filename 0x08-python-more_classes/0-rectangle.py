5ce0d8a9221a17902b43d7be7686d7bde7fb9642
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-01-05 05:21:17 +0100

    create an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 
+"""A module to test the functionality of the python class feature
+
+This module contains a simple class called Rectangle"""
+
+
 class Rectangle:
+    """An empty class object
+
+    carries NO arguments"""
     pass
