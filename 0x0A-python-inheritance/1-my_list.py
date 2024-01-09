b3a199996fca880aaccf4bb67c247a2e94449595
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-01-09 01:33:08 +0100

    docstring fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,13 @@ Module contains the class MyList that inherits from the Base class 'list'"""
 
 
 class MyList(list):
+    """a CLASS THAT INHERITS FROM THE bASE CLASS LIST
+
+        ARGS:
+            list"""
     def print_sorted(self):
+        """Method that Prints a sorted list,
+        wiithout affecting the main list"""
         sorted = self[:]
         sorted.sort()
         print(sorted)
