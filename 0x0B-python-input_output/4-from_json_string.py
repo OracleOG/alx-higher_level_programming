d384f4b52e235efaaef8d353b41c20e02d3eca1e
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-01-10 05:18:24 +0100

    fix pycodestyle error

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+import json
+
 """ module that contains a function that returns on object represented
 by a JSON string
 """
-import json
 
 
 def from_json_string(my_str):
     """Function that returns an object represented by a JSON string
-    
-        Args: 
+
+        Args:
             my_str - json formatted string"""
     return json.loads(my_str)
