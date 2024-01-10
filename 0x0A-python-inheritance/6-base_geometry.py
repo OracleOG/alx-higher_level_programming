bdbae01b0714d81390d14f4602d67067403bbc51
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-01-10 17:00:00 +0100

    enable class to raise an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Module that demonstrates the functionality of the Python's 'inheritance'
+
+    Contains the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
