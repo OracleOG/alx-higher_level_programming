fca2e6340fc9047821b8791f21e79abc05126185
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-01-11 05:35:39 +0100

    use the inetger_validator method on the rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Module that demonstrates the functionality of the Python's 'inheritance'
+
+    Contains the class BaseGeometry"""
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from 'BaseGeometry', it defines the object Rectangle.
+    
+        Args: width, height"""
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
