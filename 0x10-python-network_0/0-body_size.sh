c18f97bf93eef815daf0ab8782c71731d25ed2cb
OracleOG <emmanuelnwafor212@gmail.com>
2024-04-13 23:47:31 +0100

    uses curl to grab response header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #displays the size of the body of the response
 
-curl -sI $1 | grep "Content-Length" | cut -d" "-f 2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f 2
