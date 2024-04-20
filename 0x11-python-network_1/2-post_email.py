9233d1a2dd1c609ff5fe11127bcae8e2a20c1111
OracleOG <emmanuelnwafor212@gmail.com>
2024-04-20 08:45:33 +0100

    ipi

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,7 @@
 """Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter, and
 displays the body of the response"""
-
-
-from urllib.request import urlopen, Request
-from urllib import parse
+from urllib import parse, request
 import sys
 
 
@@ -13,7 +10,7 @@ url = sys.argv[1]
 values = {'email': sys.argv[2]}
 data = parse.urlencode(values)
 data = data.encode('ascii')
-req = Request(url, data)
-with urlopen(req) as response:
+req = request.Request(url, data)
+with request.urlopen(req) as response:
     html = response.read()
     print(html.decode('utf-8'))
