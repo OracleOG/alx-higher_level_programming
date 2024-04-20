a644d09c3a4ba8b44f3716792e2e4cf9fcde681a
OracleOG <emmanuelnwafor212@gmail.com>
2024-04-20 08:31:52 +0100

    [fix] module description

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" sends a POST request to the passed URL with the email as a parameter """
+"""sends a POST request to the passed URL with the email as a parameter"""
 from urllib.request import urlopen, Request
 from urllib import parse
 import sys
@@ -13,3 +13,4 @@ req = Request(url, data)
 with urlopen(req) as response:
     html = response.read()
     print(html.decode('utf-8'))
+c
