f979912c9fdc2161e1e079c28ac13356b145d6e2
OracleOG <emmanuelnwafor212@gmail.com>
2024-04-20 08:41:14 +0100

    i

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response
-Python script that takes in a URL and an email, sends a POST
+""" Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter, and
 displays the body of the response
 """
