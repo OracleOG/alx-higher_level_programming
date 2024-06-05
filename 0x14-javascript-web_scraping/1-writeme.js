b83783d197147dec4d4dd60c9b5315ec9af4d12d
OracleOG <emmanuelnwafor212@gmail.com>
2024-06-05 08:46:44 +0100

    writes to file ASYNC

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+});
