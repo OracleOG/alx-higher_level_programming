7b77e05e9764b185f9bd73d372b393659ee29f44
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-22 14:25:54 +0100

    print the first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,11 +3,11 @@
 const { argv } = require('process');
 
 if (process.argv[2] === undefined) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   argv.forEach((val, index) => {
     if (index > 1) {
       console.log(val);
     }
-  })
+  });
 }
