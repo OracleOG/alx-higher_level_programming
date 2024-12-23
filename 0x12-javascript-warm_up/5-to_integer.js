ad42d681fdf84acc1f5fa2c433169d7398c19d90
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-23 09:24:39 +0100

    print number if it exists

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const strNumber = process.argv[2];
-const newNumber = Number(strNumber);
+const newNumber = Number.parseInt(strNumber);
 
 if (isNaN(newNumber)) {
   console.log('Not a number');
 } else {
-  console.log(Number(strNumber));
+  console.log(newNumber);
 }
