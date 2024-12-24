0069204e3d251c6d66d56b65140a4054e0bb6ba1
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-24 09:12:12 +0100

    computes dictionary from userIDs

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+
+const sortedDict = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (!sortedDict[value]) {
+    sortedDict[value] = [];
+  }
+  sortedDict[value].push(key);
+}
+console.log(sortedDict);
