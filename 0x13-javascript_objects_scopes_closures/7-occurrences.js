3f10e439f0cc6e5181283fe77acd76973909b13a
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-24 03:40:20 +0100

    inherit class square from class rectangle

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
