12477b5db58943ebf3184c701e2c45e6a0dba1a1
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-25 12:55:20 +0100

    prints characters of a star wars movie in ORDER

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -17,29 +17,26 @@ request(url, (error, response, body) => {
 
   const data = JSON.parse(body);
   const characters = data.characters;
-  let count = 0; // Counter to track how many character requests have been completed
-
-  characters.forEach((character, index) => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error(`Error fetching character data: ${error}`);
-      } else if (response.statusCode === 200) {
-        const characterData = JSON.parse(body);
-        // Here we add the character name to an array at the index it corresponds to in the characters array
-        const names = [];
-        names[index] = characterData.name;
-        
-        // Increment the count each time a character is successfully fetched
-        count++;
-        
-        // Check if all characters have been processed
-        if (count === characters.length) {
-          // Log all names in the correct order
-          names.forEach(name => {
-            if (name) console.log(name); // Only log if the name was set
-          });
+  
+  fetchCharacter = (url) => {
+    return ( new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode === 200) {
+          resolve(JSON.parse(body).name);
         }
-      }
+      });
+  }))
+  };
+
+  Promise.all(characters.map(fetchCharacter))
+    .then((names) => {
+      names.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.error(`Error fetching character data: ${error}`);
     });
-  });
 });
