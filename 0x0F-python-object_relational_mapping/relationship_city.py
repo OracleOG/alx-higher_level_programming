3a7aa5d9beddd82ef6124a17248c5f7e848ff165
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-28 20:31:27 +0100

    add relationship to state object for cities

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,14 +4,13 @@ using the SQLAlchemy ORM module
 """
 
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
-class State(Base):
+class City(Base):
     """Class representing the State table."""
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
