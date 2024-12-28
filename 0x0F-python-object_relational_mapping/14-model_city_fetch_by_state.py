a94813e04d413dc51c849d4d07b58c53a20240e4
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-28 17:08:09 +0100

    create City object and its listing method

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Script to list all City objects from the database hbtn_0e_6_usa"""
+
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+
+def list_cities(username, password, database):
+    """Function to list all city objects from the database"""
+    # Create engine to connect to the database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}'
+                           f'@localhost:3306/{database}', pool_pre_ping=True)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all city objects and sort by cities.id in ascending order
+    cities = session.query(City).order_by(City.id).all()
+
+    # Print the results
+    for city in cities:
+        print(f"{city.id}: {city.name}")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_cities(username, password, database)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,14 +4,12 @@ using the SQLAlchemy ORM module
 """
 
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
-Base = declarative_base()
-
-
-class State(Base):
+class City(Base):
     """Class representing the State table."""
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
