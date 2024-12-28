7740f8b48b130b80d926986906044b10bab30cff
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-28 20:00:59 +0100

    create City object and its listing method

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,12 +4,14 @@ using the SQLAlchemy ORM module
 """
 
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class City(Base):
+Base = declarative_base()
+
+
+class State(Base):
     """Class representing the State table."""
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
