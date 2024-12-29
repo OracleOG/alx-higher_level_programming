90bbcadc874bc4cbd4a3be75d6c6d0cb28d7cc23
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-29 05:14:13 +0100

    task 101

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship(
-        "City", backref="state", cascade="all, delete-orphan")
+        "City", back_populates="state", cascade="all, delete-orphan")
