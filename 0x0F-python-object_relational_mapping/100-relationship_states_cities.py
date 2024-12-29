c13dfe43bb0fa195f1f38b641e700630a04a8872
OracleOG <emmanuelnwafor212@gmail.com>
2024-12-29 04:19:07 +0100

    task 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
 
     new_state = State(name='California')
     new_city = City(name='San Francisco', state_id=new_state.id)
+    new_state.cities.append(new_city)
     session.add(new_state, new_city)
 
     session.commit()
